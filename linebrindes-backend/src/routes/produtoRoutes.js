8dcead237c55513639918f16624b118072efa91a
Lucas-SousaS <lucassousa4019@gmail.com>
2026-02-10 14:31:37 -0300

    chore: remove arquivos enviados por engano e reorganiza repositório

--- linebrindes-backend/src/routes/produtoRoutes.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-const controller = require("../controllers/produtoController");
-
-router.post("/", controller.criarProduto);
-router.get("/", controller.listarProdutos);
-router.get("/:id", controller.buscarProduto);
-
-module.exports = router;
